ccaa818ed989a44aa13eacacc68a82294c9ba95f
gwolanski <gwol@udel.edu>
2024-05-27 18:58:24 -0600

    jk

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src',
+  publicDir: 'public',
+  build: {
+    outDir: '../dist'
+  },
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './tests/setup.js',
   },
+  server: {
+    open: true
+  }
 })
