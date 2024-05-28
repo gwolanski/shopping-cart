d4349e64b57a0ed66760ddfa4611bf0d34abd1cb
gwolanski <gwol@udel.edu>
2024-05-28 08:42:46 -0600

    clean up for cloudflare deploy

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider, BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './styles/index.css';
 import routes from './routes';
-import App from "./App";
-import Home from "./Home";
-import Catalog from "./Catalog";
-import ShoppingCart from "./ShoppingCart";
 
-// const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="catalog" element={<Catalog />} />
-          <Route path="shoppingCart" element={<ShoppingCart />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
